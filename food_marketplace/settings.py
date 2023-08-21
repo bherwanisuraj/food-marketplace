a06654514c427bcab06e459629c7c45677da1a83
bherwanisuraj <bherwanisur@gmail.com>
2023-08-21 14:11:44 +0530

    added email configurations

--- food_marketplace/settings.py
+++ food_marketplace/settings.py
@@ -136,3 +136,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Email Configurations
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', cast=str)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
