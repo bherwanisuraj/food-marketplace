f1ff69077e29114a1c44b3afb27f96cd3f073cae
bherwanisuraj <bherwanisur@gmail.com>
2023-08-29 11:31:59 +0530

    made sidebar and banner a component and fixed urls

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from accounts.views import myAccount
 
 urlpatterns = [
+    path('', myAccount, name='my-account'),
     path('customer-dashboard/', views.customerDashboard, name='customer-dashboard'),
 ]
--- vendor/urls.py
+++ vendor/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from accounts.views import myAccount
 
 urlpatterns = [
+    path('', myAccount, name='my-account'),
     path('vendor-dashboard/', views.vendorDashboard, name='vendor-dashboard'),
 ]
