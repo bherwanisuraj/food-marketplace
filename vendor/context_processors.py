f168bda3d9ee55f7a97cf0747cc4eeb901eb8606
bherwanisuraj <bherwanisur@gmail.com>
2023-08-31 16:11:06 +0530

    Fixed annonymouse user error for context processor

--- vendor/context_processors.py
+++ vendor/context_processors.py
@@ -1,5 +1,10 @@
 from .models import Vendor
 
 def get_vendor(request):
-    vendor = Vendor.objects.get(vendor=request.user)
+    try:
+        vendor = Vendor.objects.get(vendor=request.user)
+        
+    except:
+        vendor = None
+    
     return dict(vendor=vendor)
