5254b4d200f06e711e36518cbb6d393221caddd8
bherwanisuraj <bherwanisur@gmail.com>
2023-09-09 12:14:12 +0530

    make one address field

--- vendor/forms.py
+++ vendor/forms.py
@@ -6,12 +6,16 @@ class VendorProfileForm(forms.ModelForm):
     # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
     # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
 
+
     class Meta:
         model = UserProfile
-        fields = ["profile_picture", "cover_picture", "address_line_1", "address_line_2", "city", "state", "country", "pincode", "longitude", "latitude"]
+        fields = ["profile_picture", "cover_picture", "address", "city", "state", "country", "pincode", "longitude", "latitude"]
 
     def __init__(self, *args, **kwargs):
         super(VendorProfileForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             if field == 'longitude' or field == 'latitude':
                 self.fields[field].widget.attrs['readonly'] = 'readonly'
+
+            elif field == 'address':
+                self.fields[field].widget.attrs['placeholder'] = 'Start typing...'
